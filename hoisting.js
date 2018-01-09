fbe8d702b4eb78eca6613b13e28219d77a915ffb
Chris Scanlon <chris.scanlon72@gmail.com>
2018-01-09 12:09:02 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -23,11 +23,12 @@ function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
-  sayMy();
+  //sayMy();
 
   function sayMy() {
     var name = "Kristin";
     console.log(name);
     // var name = "Kristin";
   }
+  sayMy();
 }
